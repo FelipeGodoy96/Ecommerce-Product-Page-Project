f4414949ae4df2a0f6a2e38cef44471a6c203659
Felipe Godoy <felipe-est@hotmail.com>
2022-11-05 16:32:15 -0300

    fix(galley): must finish fix responsive gallery

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,59 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Container from '@mui/material/Container';
 import { Divider } from '@mui/material';
+import Gallery from './components/Carousel&Lightbox';
+import Box from '@mui/material/Box'
 
 
 function App() {
-  
+  const view = {
+    position: 'relative',
+    marginTop: '7.5%',
+    width: '45%',
+    transform: 'translateX(-15%)'
+  }
+  // position: "relative", marginTop: "7.5%", width: "45%", transform: "translateX(-15%)"
 
   return (
     <Container maxWidth="xl" disableGutters>
       <Navbar/>
       <Divider/>
-      <Container>
-        
+      <Container disableGutters sx={{display: "flex", position: 'absolute', width: '100%'}}>
+        <Box sx={{
+          position: {
+            xs: 'relative',
+            sm: 'relative',
+            md: 'relative',
+            lg: 'relative',
+            xl: 'relative'
+          },
+          marginTop: {
+            xs: '7.5%',
+            sm: '7.5%',
+            md: '7.5%',
+            lg: '7.5%',
+            xl: '7.5%'
+          },
+          width: {
+            xs: '100%',
+            sm: '100%',
+            md: '45%',
+            lg: '45%',
+            xl: '45%'
+          },
+          transform: {
+            xs: 'translateY(-15%)',
+            sm: 'translateY(-15%)',
+            md: 'translateY(-15%)',
+            lg: 'translateY(-15%)',
+            xl: 'translateY(-15%)'
+          },
+          }}>
+          <Gallery/>
+        </Box>
+        <Box sx={{position: "relative", margin: "7.5%"}}>
+          
+        </Box>
       </Container>
     </Container>
   )
---
+++ src/components/Carousel&Lightbox.jsx
@@ -0,0 +1,156 @@
+import ImageGallery from "react-image-gallery";
+import React from "react";
+import './carousel&lightbox.css'
+
+export default class Gallery extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      showIndex: false,
+      showBullets: false,
+      infinite: true,
+      showThumbnails: true,
+      showFullscreenButton: false,
+      showGalleryFullscreenButton: false,
+      showPlayButton: false,
+      showGalleryPlayButton: false,
+      showNav: false,
+      isRTL: false,
+      slideDuration: 450,
+      slideInterval: 2000,
+      slideOnThumbnailOver: false,
+      thumbnailPosition: 'bottom',
+      showVideo: {},
+      useWindowKeyDown: false,
+    }
+
+    this.images = [
+      {
+        original: 'src/assets/images/image-product-1.jpg',
+        thumbnail:  'src/assets/images/image-product-1-thumbnail.jpg'
+      },
+      {
+        original: 'src/assets/images/image-product-2.jpg',
+        thumbnail: 'src/assets/images/image-product-2-thumbnail.jpg'
+      },
+      {
+        original: 'src/assets/images/image-product-3.jpg',
+        thumbnail: 'src/assets/images/image-product-3-thumbnail.jpg'
+      },
+      {
+        original: 'src/assets/images/image-product-4.jpg',
+        thumbnail: 'src/assets/images/image-product-4-thumbnail.jpg'
+      },
+    ]
+  }
+
+  _onImageClick(event) {
+    console.debug('clicked on image', event.target, 'at index', this._imageGallery.getCurrentIndex())
+  }
+
+  _onImageLoad(event) {
+    console.debug('loaded image', event.target.src);
+  }
+
+  _onSlide(index) {
+    this._resetVideo();
+    console.debug('slid to index', index);
+  }
+
+  _onPause(index) {
+    console.debug('paused on index', index);
+  }
+
+  _onScreenChange(fullScreenElement) {
+    console.debug('isFullScreen?', !!fullScreenElement);
+  }
+
+  _onPlay(index) {
+    console.debug('playing from index', index);
+  }
+
+  _handleInputChange(state, event) {
+    if (event.target.value > 0) {
+      this.setState({[state]: event.target.value});
+    }
+  }
+
+  _handleCheckboxChange(state, event) {
+    this.setState({[state]: event.target.checked});
+  }
+
+  _handleThumbnailPositionChange(event) {
+    this.setState({thumbnailPosition: event.target.value});
+  }
+
+  _getStaticImages() {
+    let images = [];
+    for (let i = 2; i < 12; i++) {
+      images.push({
+        original: `${PREFIX_URL}${i}.jpg`,
+        thumbnail:`${PREFIX_URL}${i}t.jpg`
+      });
+    }
+
+    return images;
+  }
+
+  _resetVideo() {
+    this.setState({showVideo: {}});
+
+    if (this.state.showPlayButton) {
+      this.setState({showGalleryPlayButton: true});
+    }
+
+    if (this.state.showFullscreenButton) {
+      this.setState({showGalleryFullscreenButton: true});
+    }
+  }
+
+  _toggleShowVideo(url) {
+    const showVideo = this.state;
+    this.setState({
+      showVideo: {
+        ...showVideo,
+        [url]: !showVideo[url]
+      }
+    });
+
+    if (!showVideo[url]) {
+      if (this.state.showPlayButton) {
+        this.setState({showGalleryPlayButton: false});
+      }
+
+      if (this.state.showFullscreenButton) {
+        this.setState({showGalleryFullscreenButton: false});
+      }
+    }
+  }
+
+  render () {
+    return <ImageGallery          
+    ref={i => this._imageGallery = i}
+    items={this.images}
+    onClick={this._onImageClick.bind(this)}
+    onImageLoad={this._onImageLoad}
+    onSlide={this._onSlide.bind(this)}
+    onPause={this._onPause.bind(this)}
+    onScreenChange={this._onScreenChange.bind(this)}
+    onPlay={this._onPlay.bind(this)}
+    infinite={this.state.infinite}
+    showBullets={this.state.showBullets}
+    showFullscreenButton={this.state.showFullscreenButton && this.state.showGalleryFullscreenButton}
+    showPlayButton={this.state.showPlayButton && this.state.showGalleryPlayButton}
+    showThumbnails={this.state.showThumbnails}
+    showIndex={this.state.showIndex}
+    showNav={this.state.showNav}
+    isRTL={this.state.isRTL}
+    thumbnailPosition={this.state.thumbnailPosition}
+    slideDuration={parseInt(this.state.slideDuration)}
+    slideInterval={parseInt(this.state.slideInterval)}
+    slideOnThumbnailOver={this.state.slideOnThumbnailOver}
+    additionalClass="app-image-gallery"
+    useWindowKeyDown={this.state.useWindowKeyDown} />
+  }
+}
